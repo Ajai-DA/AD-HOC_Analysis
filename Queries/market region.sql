bd42ad4ea23ecaaec3247d93ff388821629f1c56
AJAI TIWARI <tiwari.ajai007@gmail.com>
2024-07-13 22:06:59 +0530

    Delete Queries directory

--- Queries/market region.sql
+++
@@ -1,5 +0,0 @@
-SELECT distinct
-customer,market,region
-from dim_customer
-where customer="ATLIQ EXCLUSIVE"
-and    region="APAC";
--- Queries/max_min_manu_cost.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-m.product_code,p.product,m.manufacturing_cost
- FROM fact_manufacturing_cost m
- join dim_product p
- on p.product_code=m.product_code
- where manufacturing_cost in(
-							(select min(manufacturing_cost) from fact_manufacturing_cost ),
-                            (select max(manufacturing_cost) from fact_manufacturing_cost)
-                            );
